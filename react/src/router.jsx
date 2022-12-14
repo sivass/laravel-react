902854c5633f5985cdb7d31150d2e819f181c7a4
sivass <sivass2703@gmail.com>
2022-12-14 22:39:46 +0800

    Setup Laravel and React Application

---
+++ react/src/router.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { createBrowserRouter, Navigate } from 'react-router-dom';
+import DefaultLayout from './components/DefaultLayout.jsx';
+import GuestLayout from './components/GuestLayout.jsx';
+import Dashboard from './views/Dashboard.jsx';
+import Login from './views/Login.jsx';
+import NotFound from './views/NotFound.jsx';
+import Signup from './views/Signup.jsx';
+import Users from './views/Users.jsx';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: '/',
+                element: <Navigate to="/users" />
+            },
+            {
+                path: '/users',
+                element: <Users />
+            },
+            {
+                path: '/dashboard',
+                element: <Dashboard />
+            }
+        ]
+    },
+    {
+        path: '/',
+        element: <GuestLayout />,
+        children: [
+            {
+                path: '/login',
+                element: <Login />
+            },
+            {
+                path: '/signup',
+                element: <Signup />
+            },
+        ]
+    },
+    {
+        path: '*',
+        element: <NotFound />
+    },
+
+])
+
+export default router
